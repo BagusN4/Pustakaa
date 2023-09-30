232774c08637557f0d3c67318783196c236b2b77
BagusN4 <145647697+BagusN4@users.noreply.github.com>
2023-10-01 02:13:33 +0700

    Add files via upload

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    Public function index()
+    {
+        echo  "<h2>Perkenalan<h2>";
+        echo  "Nama Saya Bagus
+            Saya tinggal di Bogor";
+    }
+}
